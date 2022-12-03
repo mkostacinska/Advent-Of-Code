910de85cb26cdac4b5f10a78e1264278363a275b
Maja Kostacinska <maja.kkostacinska@gmail.com>
2022-12-03 00:19:57 +0000

    DAY 1:
    December 1!

---
+++ solutions/day1.py
@@ -0,0 +1,17 @@
+with open('./solutions/day1-input.txt') as f:
+    lines = f.readlines()
+
+# PART 1 solution - explicitly summing up
+current = 0
+all = []
+for line in lines:
+    if line == '\n':
+        all.append(current)
+        current = 0
+        continue
+    current += int(line.rstrip('\n'))
+
+print('PART1: ' + str(max(all)))
+
+# PART 2 solution:
+print('PART2: '+str(sum(list(reversed(sorted(all)))[:3])))
